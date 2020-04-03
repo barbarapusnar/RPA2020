23ce6626fb77125caf6809e1dbfa11160fcdd979
Barbara Pusnar <barbara.pusnar@scng.si>
2020-04-03 12:17:15 +0200

    Dodan edit

--- DrugaMVC/DrugaMVC/Controllers/StudentController.cs
+++ DrugaMVC/DrugaMVC/Controllers/StudentController.cs
@@ -9,10 +9,11 @@ namespace DrugaMVC.Controllers
 {
     public class StudentController : Controller
     {
+        List<Student> seznam = new List<Student>();
         // GET: Student
         public ActionResult Index()
         {
-            var seznam = new List<Student> {
+                seznam = new List<Student> {
                 new Student(){ StudentID=1,StudentIme="Janez",Leta=18},
                 new Student(){ StudentID=2,StudentIme="Metka",Leta=22},
                 new Student(){ StudentID=3,StudentIme="Francka",Leta=20},
@@ -30,6 +31,30 @@ namespace DrugaMVC.Controllers
             { StudentID = 1, StudentIme = "Janez", Leta = 18 };
             return View(x);
 
+        }
+        public ActionResult Edit(int id)
+        {
+            seznam = new List<Student> {
+                new Student(){ StudentID=1,StudentIme="Janez",Leta=18},
+                new Student(){ StudentID=2,StudentIme="Metka",Leta=22},
+                new Student(){ StudentID=3,StudentIme="Francka",Leta=20},
+            };
+            Student x = seznam.Where(a => a.StudentID == id).FirstOrDefault();
+            //drugi način
+            //var y = (from a in seznam
+            //         where a.StudentID == id
+            //         select a).FirstOrDefault();
+            return View(x);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student a)
+        {
+            Student x = new Student();
+            x.StudentID = a.StudentID;
+            x.StudentIme = a.StudentIme;
+            x.Leta = a.Leta;
+            //posodobi podatkovno bazo
+            return RedirectToAction("Index");
         }
     }
 }
