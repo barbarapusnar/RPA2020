164684c208a82d904499022c7315122ba1faf214
Barbara Pusnar <barbara.pusnar@scng.si>
2020-04-20 09:21:03 +0200

    javascript

--- MVCinWebAPI/MVCinWebAPI/Controllers/HomeController.cs
+++ MVCinWebAPI/MVCinWebAPI/Controllers/HomeController.cs
@@ -8,6 +8,14 @@ namespace MVCinWebAPI.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Admin()
+        {
+            string apiUri = Url.HttpRouteUrl("DefaultApi", 
+                new { controller = "admin" });
+            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
+            return View();
+
+        }
         public ActionResult Index()
         {
             return View();
