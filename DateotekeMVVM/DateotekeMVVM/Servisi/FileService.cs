c18ba264bc9c1e3b63ebed9854b8eba97bea7ede
Barbara Pusnar <barbara.pusnar@scng.si>
2020-06-01 09:22:54 +0200

    MVVM v akciji

---
+++ DateotekeMVVM/DateotekeMVVM/Servisi/FileService.cs
@@ -0,0 +1,30 @@
+﻿using DateotekeMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DateotekeMVVM.Servisi
+{
+    class FileService
+    {
+        public async Task SaveAsync(FileInfo model)
+        {
+            if (model != null)
+            {
+                await FileIO.WriteTextAsync(model.Ref, model.Text);
+            }
+        }
+        public async Task<FileInfo> LoadAsync(StorageFile f)
+        {
+            return new FileInfo
+            {
+                Text = await FileIO.ReadTextAsync(f),
+                Name = f.DisplayName,
+                Ref = f
+            };
+        }
+    }
+}
---
+++ DateotekeMVVM/DateotekeMVVM/ViewModel/MojViewModel.cs
@@ -0,0 +1,47 @@
+﻿using DateotekeMVVM.Model;
+using DateotekeMVVM.Servisi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+
+namespace DateotekeMVVM.ViewModel
+{
+    class MojViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private FileInfo _dat;
+        public FileInfo Dat
+        {
+            get { return _dat; }
+            set {
+                _dat = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Dat)));
+            }
+        }
+        FileService _fs = new FileService();
+        public async void Shrani()
+        {
+            await _fs.SaveAsync(Dat);
+        }
+        public async void Odpri()
+        {
+            FileOpenPicker op = new FileOpenPicker();
+            op.ViewMode = PickerViewMode.Thumbnail;
+            op.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            op.FileTypeFilter.Add(".txt");
+            op.FileTypeFilter.Add(".cs");
+            StorageFile f = await op.PickSingleFileAsync();
+            if (f == null)
+                await new MessageDialog("Nobena datoteka ni izbrana").ShowAsync();
+            else
+                Dat =await _fs.LoadAsync(f);
+        }
+
+    }
+}
