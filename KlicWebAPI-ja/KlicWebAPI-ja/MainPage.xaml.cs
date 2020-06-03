141432fcf4365df48794332f7948f279e8764a34
Barbara Pusnar <barbara.pusnar@scng.si>
2020-06-03 10:17:30 +0200

    Klic

---
+++ KlicWebAPI-ja/KlicWebAPI-ja/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace KlicWebAPI_ja
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<Slika> vsiPsi;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            vsiPsi = new ObservableCollection<Slika>();
+        }
+
+        private async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await Klic.PopulatePsi(vsiPsi);
+        }
+    }
+}
---
+++ KlicWebAPI-ja/KlicWebAPI-ja/Podatki.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlicWebAPI_ja
+{
+
+    public class Podatki
+    {
+        public string[] message { get; set; }
+        public string status { get; set; }
+    }
+    public class Slika
+    {
+        public string Pot { get; set; }
+    }
+    public class Klic
+    {
+        public static async Task PopulatePsi(ObservableCollection<Slika> poti)
+        {
+            string url = "https://dog.ceo/api/breed/bullterrier/staffordshire/images";
+            Podatki p = new Podatki();
+            using (var klient = new HttpClient())
+            {
+                HttpResponseMessage sp = await klient.GetAsync(url);
+                p = await sp.Content.ReadAsAsync<Podatki>();
+            }
+            int k = 0;
+            foreach (string x in p.message)
+            {
+                Slika s = new Slika();
+                s.Pot = x;
+                poti.Add(s);
+                k++;
+                //if (k >= 10) break;
+            }
+        }
+    }
+}
